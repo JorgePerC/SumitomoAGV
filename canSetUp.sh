dc2b01522165828bd5956cff5a1353c8f0a0d626
sumitomo <sumitomo@jetson.sumitomo>
2022-09-06 20:29:58 -0500

    can script configuration

---
+++ canSetUp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+bitrate=1000000
+echo "Setting CAN bus Up"
+
+echo "Bitrate is: $bitrate"
+sudo modprobe can
+sudo modprobe can_raw
+sudo modprobe mttcan
+
+sudo ifconfig can0 txqueuelen 10000
+
+echo "Services are up"
+
+sudo ip link set can0 type can bitrate $bitrate 
+
+sudo ip link set can0 up
+
